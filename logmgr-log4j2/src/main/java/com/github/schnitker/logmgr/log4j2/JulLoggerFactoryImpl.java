2ed13eda61b4adfaab2203b20d30d65174fea324
Rainer Schnitker <Rainer.Schnitker@arcor.de>
2014-09-23 18:35:43 +0200

    test classloader

--- logmgr-log4j2/src/main/java/com/github/schnitker/logmgr/log4j2/JulLoggerFactoryImpl.java
+++ logmgr-log4j2/src/main/java/com/github/schnitker/logmgr/log4j2/JulLoggerFactoryImpl.java
@@ -1,17 +1,24 @@
 package com.github.schnitker.logmgr.log4j2;
 
-import java.util.logging.Logger;
-
 import com.github.schnitker.logmgr.JulLoggerFactory;
 
 /**
  * Implementation for logger factory. <p>
  * Requires that JulServletContextListener initialize the value isStarted. Add to web.xml for servlet api <= 2.5.
+ * 
  * @author Schnitker
  */
 public class JulLoggerFactoryImpl implements JulLoggerFactory {
 
+    // singleton per classloader
     static boolean isStarted;
+    
+    public JulLoggerFactoryImpl() {
+        // test simple java app: same classloader
+        if (getClass().getClassLoader().equals(JulLoggerFactory.class.getClassLoader())) {
+            isStarted = true;
+        }
+    }
     
     @Override
     public boolean isConfigured() {
@@ -19,7 +26,7 @@ public class JulLoggerFactoryImpl implements JulLoggerFactory {
     }
     
     @Override
-    public Logger getLogger ( String name ) {
+    public java.util.logging.Logger getLogger ( String name ) {
         return new JulLoggerWrapper ( name );
     }
 }
